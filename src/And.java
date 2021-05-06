6af5895c97426bb5085fd0e62d3466f3694cd859
Artiom Divak <artten@gmail.com>
2021-05-06 22:31:45 +0300

    started working on and

--- src/And.java
+++ src/And.java
@@ -9,26 +9,36 @@ import java.util.Map;
  *
  */
 public class And {
+    Expression left;
+    Expression right;
     // Evaluate the expression using the variable values provided
     // in the assignment, and return the result. If the expression
     // contains a variable which is not in the assignment, an exception
     // is thrown.
-    Boolean evaluate(Map<String, Boolean> assignment) throws Exception{
+    Boolean evaluate(Map<String, Boolean> assignment) throws Exception {
 
     }
 
     // A convenience method. Like the `evaluate(assignment)` method above,
     // but uses an empty assignment.
-    Boolean evaluate() throws Exception;
+    Boolean evaluate() throws Exception {
+
+    }
 
     // Returns a list of the variables in the expression.
-    List<String> getVariables();
+    List<String> getVariables() {
+
+    }
 
     // Returns a nice string representation of the expression.
-    String toString();
+    String toString() {
+
+    }
 
     // Returns a new expression in which all occurrences of the variable
     // var are replaced with the provided expression (Does not modify the
     // current expression).
-    Expression assign(String var, Expression expression)
+    Expression assign(String var, Expression expression) {
+
+    }
 }
